4f701f514d04192fd78e640131e6efe6a8e6ea7f
GoArmGo <theuniverse898@gmail.com>
2025-05-19 17:18:22 +0700

    renamed task rep

---
+++ internal/repository/task_repository.go
@@ -0,0 +1,14 @@
+package repository
+
+import (
+	"context"
+	"task-microservice/internal/model"
+)
+
+type TaskRepository interface {
+	Create(ctx context.Context, task *model.Task) error
+	GetByID(ctx context.Context, id int64) (*model.Task, error)
+	GetAll(ctx context.Context) ([]*model.Task, error)
+	Update(ctx context.Context, task *model.Task) error
+	Delete(ctx context.Context, id int64) error
+}
